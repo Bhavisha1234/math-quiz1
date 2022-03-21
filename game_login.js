b3e728cc6bfe41f46eaf9b621f0b2862b54c9d79
Bhavisha1234 <88883356+Bhavisha1234@users.noreply.github.com>
2022-03-21 17:06:30 +0800

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function add_user(){
+    player1_name=document.getElementById("player1_login").value;
+    player2_name=document.getElementById("player2_login").value; 
+    localStorage.setItem("player1" , player1_name);
+    localStorage.setItem("player2" , player2_name);
+    window.location='game_page.html';
+  }
